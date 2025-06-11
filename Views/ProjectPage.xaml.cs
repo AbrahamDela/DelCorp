aca3e237169d98732bd72d6590603e3fa0f7941d
AbrahamDela <107351899+AbrahamDela@users.noreply.github.com>
2025-06-10 22:32:14 -0600

    Add project deletion from detail view

--- Views/ProjectPage.xaml.cs
+++ Views/ProjectPage.xaml.cs
@@ -4,9 +4,22 @@ namespace DelCorp.Views;
 
 public partial class ProjectPage : ContentPage
 {
-	public ProjectPage(ProjectViewModel projectViewModel)
-	{
-		InitializeComponent();
-        BindingContext = projectViewModel;
+    private readonly ProjectViewModel _viewModel;
+
+    public ProjectPage(ProjectViewModel projectViewModel)
+    {
+        InitializeComponent();
+        _viewModel = projectViewModel;
+        BindingContext = _viewModel;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (_viewModel != null && _viewModel.RefreshProjectsCommand.CanExecute(null))
+        {
+            await _viewModel.RefreshProjectsCommand.ExecuteAsync(null);
+        }
     }
 }
